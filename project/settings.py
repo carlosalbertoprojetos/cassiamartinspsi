087c22d079ac91b52b8388608b2eb27cf3b59f51
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2025-01-21 15:29:00 -0300

    staticfiles 2

--- project/settings.py
+++ project/settings.py
@@ -136,7 +136,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "project/static"),)
 
