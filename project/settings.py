05e97f840005eddabc9894dd7590ce942e6893cb
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2025-01-23 22:52:02 -0300

    commitando atualização settings

--- project/settings.py
+++ project/settings.py
@@ -71,12 +71,12 @@ WSGI_APPLICATION = "project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
 
 
 # Configuração do Banco de Dados Postgresql
